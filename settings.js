09bd1ae2951eb216a325c8b7413041cd4c62b82c
GitHubJustForIt <151542280+GitHubJustForIt@users.noreply.github.com>
2026-05-05 17:25:17 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -67,7 +67,7 @@ const SETTINGS = {
   // ─────────────────────────────────────────────────────────
   bookingDates: [
     {
-      date: "2026-04-18",
+      date: "2026-05-8",
       slots: [
         { time: "13:00", maxPlayers: 5, visitorRate: "low"    },
         { time: "13:30", maxPlayers: 5, visitorRate: "low"    },
